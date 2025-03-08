bdd8d093159ead411a22ee232c13855c6f2aede1
Kate-Gornostaeva <itsmekate153@gmail.com>
2025-03-09 00:42:34 +1000

    weather_news

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = None
+    news = None
+
+    if request.method == 'POST':
+        city = request.form['city']
+        weather = get_weather (city)
+        news = get_news()
+    return render_template('index.html', weather=weather, news=news)
+
+def get_weather(city):
+    api_key = "794f37297fd3e407a71d6aab12aa1c5d"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    response = requests.get(url)
+    return response.json()
+
+def get_news():
+    api_key = "cc1162a387ff4e4fb895df4ccd36e91b"
+    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+    response = requests.get(url)
+    return response.json().get("articles", [])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
